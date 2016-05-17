1ff341736bcc45b7c6c9cd037a4fb423dff0aa96
Oleksiy Cherniy <lch@mentorix.dk>
2016-05-17 23:29:00 +0300

    add DomCrawler and guzzlephp

---
+++ src/CurrencyBundle/Parser/PrivatbankParser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CurrencyBundle\Parser\PrivatbankParser;
+
+use CurrencyBundle\Entity\Course;
+
+/**
+ * Class PrivatbankParser.
+ */
+class PrivatbankParser {
+
+  const PRIVATBANK_URL = 'https://privatbank.ua/';
+
+  /**
+   * GoogleUrlApi key and url service.
+   */
+  public function __construct() {
+    $this->url = self::PRIVATBANK_URL;
+  }
+
+}
